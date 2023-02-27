e0f7e86e98a224acd865315473e43c7d363c752c
“Anthony <antkameka@gmail.com>
2023-02-26 21:59:17 -0500

    simplified

--- coffee_machine/main.py
+++ coffee_machine/main.py
@@ -2,12 +2,14 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+# start instance of classes
 menu = Menu()
 cm = CoffeeMaker()
 mm = MoneyMachine()
 items = menu.get_items().split("/")[:-1]
 
 def turn_on():
+    """entire coffee machine operation, when it is turned on"""
     on = True
     while on:
         water = cm.resources["water"]
@@ -29,10 +31,7 @@ def turn_on():
             if cm.is_resource_sufficient(drink):
                 if mm.make_payment(drink.cost):
                     cm.make_coffee(drink)
-                    water -= drink.ingredients["water"]
-                    milk -= drink.ingredients["milk"]
-                    coffee -= drink.ingredients["coffee"]
-                    print(f"Here is your {drink.name} Enjoy!!")
+
 
 
 def prompt():
