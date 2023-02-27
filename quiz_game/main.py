7f651335a1f9cece9807b00f5755af79c2adee4b
“Anthony <antkameka@gmail.com>
2023-02-27 17:35:35 -0500

    add quiz game

---
+++ quiz_game/main.py
@@ -0,0 +1,19 @@
+# simple true/false quiz game
+
+from question_model import Question
+
+from data import question_data
+from quiz_brain import QuizBrain
+
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    question = Question(question_text, question_answer)
+    question_bank.append(question)
+
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_question():
+    quiz.next_question()
